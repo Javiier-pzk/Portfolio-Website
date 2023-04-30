171053f0cda709d31abe8eccc6d0d4a9ba53ea6a
Javier <javierphon@gmail.com>
2023-04-30 23:54:41 +0800

    Set padding vertical of home details page for xs screens and above to 10

--- components/homeDetails.tsx
+++ components/homeDetails.tsx
@@ -4,7 +4,7 @@ const HomePageDetails = () => {
   return (
     <section
       id="home"
-      className="max-w-cotentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4"
+      className="max-w-cotentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 xs:px-10 xl:px-4"
     >
       <motion.h3
         initial={{ y: 10, opacity: 0 }}
