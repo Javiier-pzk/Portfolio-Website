1f5c949a927f6f2d313c9f9f339ec5ba8f9bc9d1
Javier <javierphon@gmail.com>
2023-05-05 18:14:41 +0800

    Replace project url with env var

--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -12,31 +12,31 @@ const ProjectsPage = () => {
                 <ProjectCard 
                     projectTitle="Productivity Police" 
                     projectDescription="A mobile productivity app built for iOS devices that consists of some of the more important productivity features that any student might require."
-                    projectUrl="https://github.com/Javiier-pzk/Productivity-Police"
+                    projectUrl={`${process.env.NEXT_PUBLIC_GITHUB_URL}/Productivity-Police`}
                     projectTechStack={["React Native (Expo)", "Firebase"]}
                 />
                 <ProjectCard 
                     projectTitle="dfmn_bot" 
                     projectDescription="A telegram bot that helps you make decisions or provides recommendations for you."
-                    projectUrl="https://github.com/Javiier-pzk/dfmn_bot"
+                    projectUrl={`${process.env.NEXT_PUBLIC_GITHUB_URL}/dfmn_bot`}
                     projectTechStack={["pyTelegramBotAPI", "Heroku", "Google Maps Platform", "python"]}
                 />
                 <ProjectCard 
                     projectTitle="Truffle" 
                     projectDescription="A Lightweight SQL management library for C#."
-                    projectUrl="https://github.com/Javiier-pzk/Truffle"
+                    projectUrl={`${process.env.NEXT_PUBLIC_GITHUB_URL}/Truffle`}
                     projectTechStack={["C#"]}
                 />
                 <ProjectCard 
                     projectTitle="Restaurant HR Helper" 
                     projectDescription="A desktop app built as part of a school project for restaurant managers to manage their employees, suppliers and customers. It optimized for use via a Command Line Interface."
-                    projectUrl="https://github.com/Javiier-pzk/tp"
+                    projectUrl={`${process.env.NEXT_PUBLIC_GITHUB_URL}/tp`}
                     projectTechStack={["Java", "JavaFX", "Gradle"]}
                 />
                 <ProjectCard 
                     projectTitle="Duke" 
                     projectDescription="An interactive chat bot desktop app bulit as part of a school project that helps you keep track of your todos, deadlines and events."
-                    projectUrl="https://github.com/Javiier-pzk/ip"
+                    projectUrl={`${process.env.NEXT_PUBLIC_GITHUB_URL}/ip`}
                     projectTechStack={["Java", "JavaFX", "Gradle"]}
                 />
             </div>
