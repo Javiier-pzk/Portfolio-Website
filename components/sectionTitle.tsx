a51a5b7894b868bb75eda3c62d22bc04b58ca18b
Javier <javierphon@gmail.com>
2023-05-07 23:21:10 +0800

    Change height of span to 1px to support safari browsers

--- components/sectionTitle.tsx
+++ components/sectionTitle.tsx
@@ -6,15 +6,17 @@ interface Props {
 
 const SectionTitle = ({ title }: Props) => {
   return (
-    <motion.h2
+    <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 0.05 }}
-      className="font-titleFont text-2xl font-semibold flex items-center text-textYellow"
+      className="flex items-center gap-6"
     >
-      {title}
-      <span className="hidden md:flex md:w-60 lgl:w-72 h-[0.5px] bg-textDark ml-6"></span>
-    </motion.h2>
+      <h2 className="font-titleFont text-2xl font-semibold text-textYellow">
+        {title}
+      </h2>
+      <span className="hidden md:inline-flex md:w-60 lgl:w-72 h-[1px] bg-textDark"></span>
+    </motion.div>
   );
 };
 
