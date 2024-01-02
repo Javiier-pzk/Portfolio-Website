818d7a9497d28c6a77fa10b7f9aa5f2a5edeca7d
Javier <javierphon@gmail.com>
2024-01-02 23:34:48 +0800

    Remove commented code

--- components/workDetails.tsx
+++ components/workDetails.tsx
@@ -119,33 +119,6 @@ const WorkDetails = ({
         >
           {handleJobDetails(jobDetails)}
         </motion.ul>
-        {/* <div>
-          <motion.h3
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.25 }}
-            className="flex flex-col lgl:flex-row gap-1 font-medium text-xl font-titleFont"
-          >
-            {jobTitle}
-            <a href={companyUrl} target="_blank">
-              <span 
-                className="text-textYellow tracking-wide inline-flex relative cursor-pointer overflow-x-hidden group"
-              > 
-                @ {company}
-                <span className="absolute w-full h-[2px] bg-textYellow left-0 bottom-1 -translate-x-[110%] group-hover:translate-x-0 transition-transform duration-500"></span>
-              </span>
-              
-            </a>
-          </motion.h3>
-          <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.3 }}
-            className="text-md mt-1 font-medium text-textDark"
-          >
-            {jobDuration}
-          </motion.p>
-        </div> */}
       </div>
 
       <motion.ul
